458d7248fccede7f6d539dd041f0c29f7f9bc31c
James Tsay <tsay@adobe.com>
2023-06-02 10:25:25 -0700

    Fix casing

--- test/scripts/contentSecurityPolicy/csp.jest.js
+++ test/scripts/contentSecurityPolicy/csp.jest.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import ContentSecurityPolicy from '../../../acrobat/scripts/ContentSecurityPolicy/csp';
+import ContentSecurityPolicy from '../../../acrobat/scripts/contentSecurityPolicy/csp';
 
 describe('Test ContentSecurityPolicy', () => {
   let hostnameGetter;
