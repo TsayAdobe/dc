7cce2f1e868df02b7b38792d8227f19a42c8fdbc
James Tsay <tsay@adobe.com>
2023-06-05 11:12:08 -0700

    Add promotion tests

--- test/blocks/promotion/promotion.test.js
+++ test/blocks/promotion/promotion.test.js
@@ -7,14 +7,25 @@ let meta = document.createElement('meta');
 meta.name = 'promotion';
 meta.content = 'ccx-acrobat-links1';
 document.getElementsByTagName('head')[0].appendChild(meta);
-document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+let head = await readFile({ path: './mocks/head.html' });
+let body = await readFile({ path: './mocks/body.html' });
+let bodyEmpty = await readFile({ path: './mocks/body_empty.html' });
 await import('../../../acrobat/scripts/scripts');
-const { default: init } = await import(
+const { default: init, promotionFromMetadata } = await import(
   '../../../acrobat/blocks/promotion/promotion'
 );
 
-describe.skip('promotion block', () => {
-  before(async () => {
+describe('promotion block', () => {
+  beforeEach(() => {
+    document.head.innerHTML = head;    
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('shoud creates a promotion block', async () => {
+    document.body.innerHTML = body;
     const block = await waitForElement('.promotion');
     const fetchText = await readFile({ path: './mocks/fetch.html' });
     sinon.stub(window, 'fetch');
@@ -22,15 +33,41 @@ describe.skip('promotion block', () => {
       status: 200
     });
     window.fetch.returns(Promise.resolve(res));
-    await init(block);
+    await init(block);    
+    const promotion = await waitForElement('.promotion');
+    expect(promotion.textContent).to.contain('Do more with Adobe Creative Cloud.');
   });
 
-  after(() => {
-    window.fetch.restore();
-  });
-
-  it('shoud creates a promotion block', async () => {
+  it('has an empty promotion block after fetch empty', async () => {
+    document.body.innerHTML = body;
     const block = await waitForElement('.promotion');
-    expect(block.textContent).to.contain('Do more with Adobe Creative Cloud.');
+    sinon.stub(window, 'fetch');
+    var res = new window.Response('', {
+      status: 200
+    });
+    window.fetch.returns(Promise.resolve(res));
+    await init(block);    
+    const promotion = await waitForElement('.promotion');
+    expect(promotion.textContent).to.contain('');
   });
+
+  it('has an empty promotion block after fetch error', async () => {
+    document.body.innerHTML = body;
+    const block = await waitForElement('.promotion');
+    sinon.stub(window, 'fetch');
+    var res = new window.Response('File not found', {
+      status: 404
+    });
+    window.fetch.returns(Promise.resolve(res));
+    await init(block);    
+    const promotion = await waitForElement('.promotion');
+    expect(promotion.textContent).to.contain('');
+  });
+
+  it('creates a promotion block from metadata', async () => {
+    document.body.innerHTML = bodyEmpty;
+    await promotionFromMetadata('Abc');    
+    const promotion = await waitForElement('.promotion');
+    expect(promotion.dataset.promotion).to.eql('abc');
+  });    
 });
