dac5473972cc92340874675ba00541e4ab99c795
James Tsay <tsay@adobe.com>
2023-06-05 22:53:01 -0700

    Add tests for Alloy

--- test/scripts/alloy/browserExt.test.js
+++ test/scripts/alloy/browserExt.test.js
@@ -1,3 +1,16 @@
-const { init } = await import(
-  '../../../acrobat/scripts/alloy/browserExt'
-);
+import { expect } from '@esm-bundle/chai';
+import * as sinon from 'sinon';
+
+const { default: init } = await import('../../../acrobat/scripts/alloy/browserExt');
+
+describe('Alloy browserExt', () => {
+  before(() => {
+    window._satellite = {
+      track: sinon.stub(),
+    };
+  });
+
+  it('initiates Alloy browerExt', () => {
+    init('modalClosed', 'Chrome');
+  });
+});
--- test/scripts/alloy/reviewFeedback.test.js
+++ test/scripts/alloy/reviewFeedback.test.js
@@ -1,1 +1,16 @@
-const { init } = await import('../../../acrobat/scripts/alloy/reviewFeedback');
+import { expect } from '@esm-bundle/chai';
+import * as sinon from 'sinon';
+
+const { default: init } = await import('../../../acrobat/scripts/alloy/reviewFeedback');
+
+describe('Alloy reviewFeedback', () => {
+  before(() => {
+    window._satellite = {
+      track: sinon.stub(),
+    };
+  });
+
+  it('initiates Alloy browerExt', () => {
+    init('pdf-to-ppt', 2, 'test comment');
+  });
+});
