050b01bd82fd12cfd7d17d6147906812935a502f
James Tsay <tsay@adobe.com>
2023-06-06 09:04:27 -0700

    Clean up Alloy tests

--- test/scripts/alloy/review.test.js
+++ test/scripts/alloy/review.test.js
@@ -1,1 +1,27 @@
-const { init } = await import('../../../acrobat/scripts/alloy/review');
+import { expect } from '@esm-bundle/chai';
+import * as sinon from 'sinon';
+
+const { default: init } = await import(
+    '../../../acrobat/scripts/alloy/review'
+);
+
+describe('Alloy review', () => {
+  beforeEach(() => {
+    window._satellite = {
+      track: sinon.stub(),
+    };
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('initiates Alloy review', () => {
+    init();
+    expect(window._satellite.track.calledOnce).to.be.true;
+    expect(
+      window._satellite.track.args[0][1].data._adobe_corpnew.digitalData
+        .primaryEvent.eventInfo.eventName
+    ).to.equal('productRating');
+  });
+});
